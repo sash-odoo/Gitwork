9c9c9ae236393fc5db1811c852ff07aa40fa3188
sash-odoo <sash@odoo.com>
2022-02-15 10:43:24 +0530

    Git Demo

---
+++ library_management/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Library Management' , 
+    'category' : 'Sales' ,
+    'application' : True ,
+    
+    'data' : [
+        'views/library_views.xml' ,
+        'views/library_menus.xml' ,
+        'security/ir.model.access.csv',
+    ],
+
+    'license' : 'LGPL-3',
+    'installable' : True,
+}
---
+++ library_management/models/library_management.py
@@ -0,0 +1,55 @@
+from odoo import models , fields
+from odoo.exceptions import UserError,ValidationError
+from dateutil.relativedelta import relativedelta
+
+
+class LibraryManagement(models.Model) :
+    _name = "library.books"
+    _description = "Library data"
+
+    name = fields.Char()
+    description = fields.Text()
+    image = fields.Image()
+    category_id = fields.Many2one('books.category')
+    isbn = fields.Integer()
+    publisher = fields.Char()
+    Copyright = fields.Char()
+    edition = fields.Char()
+    title = fields.Char()
+    author = fields.Char()
+    price = fields.Float()
+    pages = fields.Integer()
+
+    status = fields.Selection([
+        ('available' , 'Available'),
+        ('notavailable' , 'Not Available')], default = 'available')
+
+    state = fields.Selection([
+        ('issue' , 'Issue'),
+        ('cancle' , 'Cancle')])
+
+
+    def action_issue(self) :
+        for record in self :
+            record.state = 'issue'
+
+
+
+class BookCategory(models.Model) :
+    _name = "books.category"
+    _description = "Book Category"
+
+    name = fields.Char()
+
+
+class BookRequest(models.Model) :
+    _name = "book.request"
+    _description = "Book Request"
+
+    name = fields.Char()
+    image = fields.Image()
+    author = fields.Char()
+    edition = fields.Char()
+    publisher = fields.Char()
+    request_by = fields.Many2one('res.users')
+    request_date = fields.Date(default = lambda self: fields.Datetime.now(),copy=False)
